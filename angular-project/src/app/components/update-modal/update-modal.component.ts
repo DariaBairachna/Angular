0ec1c251e6d27f4f63b4ebc33a26fd733ebabb75
Daria Bairachna <daria.bairachna@gmail.com>
2019-01-12 22:17:06 +0200

    fix validation on update

--- angular-project/src/app/components/update-modal/update-modal.component.ts
+++ angular-project/src/app/components/update-modal/update-modal.component.ts
@@ -44,13 +44,50 @@ export class UpdateModalComponent implements OnInit {
     }
   }
 
+
+  public isValidEmailAddress(emailValue: string): boolean {
+    var pattern = new RegExp(
+      /^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/
+    );
+    var result = pattern.test(emailValue);
+    return result;
+  }
+
+  public validateForm(): boolean {
+    let validateEmail = this.isValidEmailAddress(this.email);
+    if (this.name == "") {
+      alert("Name is empty!");
+      return false;
+    }
+
+    if (isNaN(this.age)) {
+      alert("Age must be number!");
+      return false;
+    }
+
+    if (this.age == null) {
+      alert("Age is empty!");
+      return false;
+    }
+
+    if (this.email == "") {
+      alert("Email is empty!");
+      return false;
+    }
+
+    if (validateEmail == false) {
+      alert("Email invalid");
+      return false;
+    }
+  }
+
+
   public updateUser(){
 
-    
-    // let validation = this.createUser.validateForm();
-    // if (validation == false) {
-    //   return false;
-    // }
+   let validation = this.validateForm();
+    if (validation == false) {
+      return false;
+    }
     this.users = this.userService.getUsers();
     let user: IUser = {
       Name: this.name,
